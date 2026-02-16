25b6656be3d042e3c792e653fe641110928d5d49
Manuel Lera-Ramirez <manulera14@gmail.com>
2026-02-16 16:59:01 +0000

    include SubtiToolKit with files in zip

--- categorise_plasmids.py
+++ categorise_plasmids.py
@@ -3,6 +3,10 @@ import json
 from pydna.parsers import parse
 import os
 import shutil
+import glob
+import re
+import warnings
+import zipfile
 
 
 def merge_syntaxes(syntax_file1: str, syntax_file2: str) -> Syntax:
@@ -43,69 +47,94 @@ for syntax_entry in index:
     else:
         syntax_files = [syntax_file]
 
-    for syntax_index, syntax_file in enumerate(syntax_files):
-        syntax = Syntax.model_validate_json(open(syntax_file).read())
+    if len(syntax_entry["kits"]) > 0:
+        for syntax_index, syntax_file in enumerate(syntax_files):
+            syntax = Syntax.model_validate_json(open(syntax_file).read())
 
-        plasmids = list()
-        for associated_kit in syntax_entry["kits"]:
-            print('>', associated_kit["kit"])
-            associated_kit: dict
-            kit_path = os.path.join("kits", associated_kit["kit"])
-            plasmid_names = associated_kit.get("names", None)
-            associated_plasmids_tsv = os.path.join(kit_path, "plasmids.tsv")
-            associated_plasmids_tsv_lines = open(associated_plasmids_tsv).readlines()
-            for line in associated_plasmids_tsv_lines[1:]:
-                ls = line.split("\t")
-                well, name, addgene_id, resistance = ls[:4]
-                content = '' if len(ls) < 5 else ls[4]
-                if plasmid_names is not None and name not in plasmid_names:
-                    continue
-                content = content.strip()
-                seq = parse(f'addgene_plasmids/{addgene_id}.gb')[0]
-                resp = syntax.assign_plasmid_to_syntax_part(seq)
-                if len(resp) == 0:
-                    print(f'Skipped plasmid {name} ({addgene_id}) because it could not be assigned to a syntax part')
-                    continue
-                elif len(resp) > 1:
-                    print(
-                        f'Skipped plasmid {name} ({addgene_id}) because it could be assigned to multiple syntax parts'
-                    )
-                    continue
-                else:
-                    resp = resp[0]
+            addgene_plasmids = list()
+            for associated_kit in syntax_entry["kits"]:
+                print('>', associated_kit["kit"])
+                associated_kit: dict
+                kit_path = os.path.join("kits", associated_kit["kit"])
+                plasmid_names = associated_kit.get("names", None)
+                associated_plasmids_tsv = os.path.join(kit_path, "plasmids.tsv")
+                associated_plasmids_tsv_lines = open(associated_plasmids_tsv).readlines()
+                for line in associated_plasmids_tsv_lines[1:]:
+                    ls = line.split("\t")
+                    well, name, addgene_id, resistance = ls[:4]
+                    content = '' if len(ls) < 5 else ls[4]
+                    if plasmid_names is not None and name not in plasmid_names:
+                        continue
+                    content = content.strip()
+                    seq = parse(f'addgene_plasmids/{addgene_id}.gb')[0]
+                    resp = syntax.assign_plasmid_to_syntax_part(seq)
+                    if len(resp) == 0:
+                        print(f'Skipped plasmid {name} ({addgene_id}) because it could not be assigned to a syntax part')
+                        continue
+                    elif len(resp) > 1:
+                        print(
+                            f'Skipped plasmid {name} ({addgene_id}) because it could be assigned to multiple syntax parts'
+                        )
+                        continue
+                    else:
+                        resp = resp[0]
 
-                left_overhang, right_overhang = resp['key'].split('-')
-                longest_feature_name = ''
-                if resp['longest_feature'] is not None and 'label' in resp['longest_feature'].qualifiers:
-                    longest_feature_name = resp['longest_feature'].qualifiers['label'][0]
-                if content != '':
-                    name += f" ({content})"
+                    left_overhang, right_overhang = resp['key'].split('-')
+                    longest_feature_name = ''
+                    if resp['longest_feature'] is not None and 'label' in resp['longest_feature'].qualifiers:
+                        longest_feature_name = resp['longest_feature'].qualifiers['label'][0]
+                    if content != '':
+                        name += f" ({content})"
 
-                plasmid = {
-                    'id': len(plasmids) + 1,
-                    'plasmid_name': name,
-                    'left_overhang': left_overhang,
-                    'right_overhang': right_overhang,
-                    'key': resp['key'],
-                    'type': 'AddgeneIdSource',
-                    'source': {
-                        'id': 1,
+                    plasmid = {
+                        'id': len(addgene_plasmids) + 1,
+                        'plasmid_name': name,
+                        'left_overhang': left_overhang,
+                        'right_overhang': right_overhang,
+                        'key': resp['key'],
                         'type': 'AddgeneIdSource',
-                        'input': [],
-                        'repository_id': addgene_id,
-                    },
-                }
-                plasmids.append(plasmid)
+                        'source': {
+                            'id': 1,
+                            'type': 'AddgeneIdSource',
+                            'input': [],
+                            'repository_id': addgene_id,
+                        },
+                    }
+                    addgene_plasmids.append(plasmid)
 
-        if not os.path.exists(f'syntaxes/{syntax_path}'):
-            os.makedirs(f'syntaxes/{syntax_path}')
-        if "syntaxes" in syntax_entry:
-            print('>', syntax_entry["syntaxes"])
-            plasmid_file_name = f'syntaxes/{syntax_path}/plasmids_{syntax_entry["syntaxes"][syntax_index]["path"]}'
-        else:
-            plasmid_file_name = f'syntaxes/{syntax_path}/plasmids.json'
-        with open(plasmid_file_name, 'w') as f:
-            json.dump(plasmids, f, indent=4)
+            if not os.path.exists(f'syntaxes/{syntax_path}'):
+                os.makedirs(f'syntaxes/{syntax_path}')
+            if "syntaxes" in syntax_entry:
+                print('>', syntax_entry["syntaxes"])
+                plasmid_file_name = f'syntaxes/{syntax_path}/plasmids_{syntax_entry["syntaxes"][syntax_index]["path"]}'
+            else:
+                plasmid_file_name = f'syntaxes/{syntax_path}/plasmids.json'
+            with open(plasmid_file_name, 'w') as f:
+                json.dump(addgene_plasmids, f, indent=4)
+    if "plasmid_files" in syntax_entry:
+        plasmid_files = [f.split("/")[-1] for f in glob.glob(os.path.join(syntax_entry["plasmid_files"], "*.gb"))]
+        for syntax_index, syntax_file in enumerate(syntax_files):
+            syntax = Syntax.model_validate_json(open(syntax_file).read())
+            filtered_plasmids = plasmid_files
+            if "filename_pattern" in syntax_entry["syntaxes"][syntax_index]:
+                filename_pattern = syntax_entry["syntaxes"][syntax_index]["filename_pattern"]
+                filtered_plasmids = [f for f in plasmid_files if re.match(filename_pattern, f)]
+            plasmids_to_export = list()
+            for plasmid_file in filtered_plasmids:
+                plasmid_file_path = os.path.join(syntax_entry["plasmid_files"], plasmid_file)
+                with warnings.catch_warnings():
+                    warnings.simplefilter("ignore")
+                    plasmid = parse(plasmid_file_path)[0]
+                resp = syntax.assign_plasmid_to_syntax_part(plasmid)
+                if len(resp) == 1:
+                    plasmids_to_export.append(plasmid_file + resp[0]['key'])
+
+            zip_filename = syntax_file[:-5] + ".zip"
+
+            with zipfile.ZipFile(zip_filename, 'w') as zipf:
+                for plasmid_file in filtered_plasmids:
+                    plasmid_file_path = os.path.join(syntax_entry["plasmid_files"], plasmid_file)
+                    zipf.write(plasmid_file_path, arcname=plasmid_file)
 
 # Write a minified index.json file
 mini_index = dict()
